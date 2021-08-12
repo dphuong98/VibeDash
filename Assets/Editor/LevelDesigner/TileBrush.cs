48dda834a14bf812bb3d102bbfae682e877ca5a0
dphuong98 <32475252+dphuong98@users.noreply.github.com>
2021-08-12 18:46:56 +0700

    TileBrush: check if TilePalette window is opened
    +TileBrush can only work with TilePalette opened

--- Assets/Editor/LevelDesigner/TileBrush.cs
+++ Assets/Editor/LevelDesigner/TileBrush.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.EditorTools;
 using UnityEditor.Tilemaps;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.WSA;
 
 public class TileBrush : EditorWindow
 {
@@ -27,21 +30,32 @@ public class TileBrush : EditorWindow
 
     private void OnGUI()
     {
-        GUIContent buttonContent;
         GUILayout.BeginHorizontal();
         foreach (var brush in brushes)
         {
             var so = new SerializedObject(brush);
             var prefab = so.FindProperty("m_Prefab").objectReferenceValue;
 
-            buttonContent = new GUIContent(AssetPreview.GetAssetPreview(prefab), brush.name);
+            var buttonContent = new GUIContent(AssetPreview.GetAssetPreview(prefab), brush.name);
             if (GUILayout.Button(buttonContent, GUILayout.Width(buttonSize.x), GUILayout.Height(buttonSize.y)))
             {
-                //TODO Even if you switch draw tool, Unity wont let you draw unless you have Tile Palette window opened
+                GetTilePaletteWindow();
                 EditorTools.SetActiveTool(typeof(PaintTool));
                 GridPaintingState.gridBrush = brush;
             }
         }
         GUILayout.EndHorizontal();
+    }
+
+    private void GetTilePaletteWindow()
+    {
+        var type = Type.GetType("UnityEditor.Tilemaps.GridPaintPaletteWindow, Unity.2D.Tilemap.Editor.dll");
+        var getWindowInfo = typeof(EditorWindow)
+            .GetMethods().First(m => m.Name == "GetWindow"
+                                     && m.IsGenericMethod
+                                     && m.GetParameters().Length > 0
+                                     && m.GetParameters()[0].ParameterType == typeof(Type[]));
+        var genericMethod = getWindowInfo.MakeGenericMethod(type);
+        genericMethod.Invoke(null, new object[] { new Type[] { typeof(TileBrush) }});
     }
 }
