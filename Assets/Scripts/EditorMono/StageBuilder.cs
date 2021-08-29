c3057749dd3d67fa36a39a96fd70b51986333460
Phương <dphuongmeo@gmail.com>
2021-08-30 04:59:59 +0700

    HighLighting selected cell

--- Assets/Scripts/EditorMono/StageBuilder.cs
+++ Assets/Scripts/EditorMono/StageBuilder.cs
@@ -12,6 +12,7 @@ public class StageBuilder : MonoBehaviour
     private const string TempFile = "Assets/Editor/LevelBuilderTmp/_tmp_.asset"; //OG named these caches but they dont behave like caches
 
     private Grid grid;
+    private Vector2Int selectedTile = new Vector2Int(-1, -1);
 
     private Stage _currentStage;
     public Stage CurrentStage
@@ -60,13 +61,99 @@ public class StageBuilder : MonoBehaviour
         }
 
         DrawTileIcons();
-        //HandleClick();
+        HandleClick();
 
         //Other GUI option
         
         //Stage info
 
         //DrawSolution
+    }
+
+    private void HandleClick()
+    {
+        if (Selection.activeGameObject == this.gameObject &&
+            Event.current.type == EventType.MouseDown &&
+            Event.current.modifiers == EventModifiers.None &&
+            Event.current.button == 0)
+        {
+            var worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+
+            if (Physics.Raycast(worldRay, out var hitInfo))
+            {
+                if (hitInfo.collider.gameObject == this.gameObject)
+                {
+                    Vector3 point = hitInfo.point;
+                    var worldPos = hitInfo.collider.gameObject.transform.InverseTransformPoint(point);
+                    var gridPos = grid.WorldToCell(worldPos);
+                    selectedTile = new Vector2Int(gridPos.x, gridPos.y);
+
+                    Event.current.Use();
+                }
+            }
+        }
+
+        /*if (Selection.activeGameObject == gameObject &&
+            Event.current.type == EventType.MouseDown &&
+            Event.current.modifiers == EventModifiers.None &&
+            Event.current.button == 1)
+        {
+            Index index = new Index();
+            if (GetClickedDot(ref index))
+            {
+                selection = index;
+                SceneView.RepaintAll();
+                CreateMenu(index).ShowAsContext();
+            }
+        }*/
+
+
+        /*if (Selection.activeGameObject == gameObject &&
+            Event.current.type == EventType.KeyDown
+            )
+        {
+
+            Level.Dot dot = null;
+
+            if (selection.r >= 0 && selection.r < editingLevel.Dimension.r && selection.c >= 0 && selection.c < editingLevel.Dimension.c)
+            {
+                dot = editingLevel[selection.r, selection.c];
+            }
+            else
+            {
+                return;
+            }
+
+            var types = ShortCuts.Where(i => i.Value == Event.current.character).Select(k => k.Key);
+            if (types.Count() > 0)
+            {
+                dot.type = types.First();
+                Event.current.Use();
+                EditorUtility.SetDirty(editingLevel);
+            }
+
+            if (Event.current.character == 'h')
+            {
+                if (dot.type == Level.DotType.Tile || dot.type == Level.DotType.Block || dot.type == Level.DotType.Empty || dot.type == Level.DotType.Redirection)
+                {
+                    dot.Index = selection;
+                    if (!EditingLevel.IsHint(dot.Index))
+                        EditingLevel.AddHint(selection);
+                    else
+                        EditingLevel.RemoveHint(selection);
+                    Event.current.Use();
+                    EditorUtility.SetDirty(editingLevel);
+                }
+            }
+
+            if (Event.current.character >= '6' && Event.current.character <= '9')
+            {
+                dot.lamp[Event.current.character - '6'] = !dot.lamp[Event.current.character - '6'];
+                Event.current.Use();
+                EditorUtility.SetDirty(editingLevel);
+            }
+
+        }*/
     }
 
     private void DrawTileIcons()
@@ -76,26 +163,48 @@ public class StageBuilder : MonoBehaviour
             for (var x = 0; x < CurrentStage.Size.x; x++)
             {
                 var tile = CurrentStage[x, y];
-                DrawTileIcon(tile, new Vector3Int(x, y, 0));
+                var gridPos = new Vector2Int(x, y);
+                
+                DrawTileIcon(tile, gridPos);
+                if (selectedTile == gridPos)
+                    DrawHighLight(gridPos);
             }
         }
         
     }
 
-    private void DrawTileIcon(TileType tile, Vector3Int gridPos)
+    private void DrawHighLight(Vector2Int gridPos)
+    {
+        Handles.color = Color.yellow;
+        var worldPos = grid.GetCellCenterWorld(new Vector3Int(gridPos.x, gridPos.y, 0));
+
+        var Radius = 0.5f;
+        var rr = Radius / 2 * Mathf.Sqrt(2) * 1.4f;
+        Handles.DrawAAPolyLine(7, new Vector3[]
+        {
+            worldPos + new Vector3(-rr, -rr),
+            worldPos + new Vector3(-rr, rr),
+            worldPos + new Vector3(rr, rr),
+            worldPos + new Vector3(rr, -rr),
+
+            worldPos + new Vector3(-rr, -rr),
+        });
+    }
+
+    private void DrawTileIcon(TileType tile, Vector2Int gridPos)
     {
         if (tile == TileType.Wall)
         {
-            var worldPos = grid.GetCellCenterWorld(gridPos);
+            var worldPos = grid.GetCellCenterWorld(new Vector3Int(gridPos.x, gridPos.y, 0));
             Handles.color = Color.green;
 
-            float ee = 0.45f;
+            float iconRadius = 0.45f;
             Handles.DrawAAConvexPolygon(new Vector3[]
             {
-                worldPos + new Vector3(-ee, -ee),
-                worldPos + new Vector3(-ee, ee),
-                worldPos + new Vector3(ee, ee),
-                worldPos + new Vector3(ee, -ee),
+                worldPos + new Vector3(-iconRadius, -iconRadius),
+                worldPos + new Vector3(-iconRadius, iconRadius),
+                worldPos + new Vector3(iconRadius, iconRadius),
+                worldPos + new Vector3(iconRadius, -iconRadius),
             });
         }
     }
